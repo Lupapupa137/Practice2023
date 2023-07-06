d48d5f2181a6916429416274b300b563c0c82726
LUPAPUPA <yana@LAPTOP-KES1F632>
2023-07-06 21:16:06 +0300

    push task1

---
+++ Task1/Student.cs
@@ -0,0 +1,148 @@
+﻿namespace Practice.Labs;
+
+public class Student:
+    IEquatable<Student>
+{
+    private string? _name; // И
+    private string? _lastName;// Ф
+    private string? _middleName; // О
+
+    private string? _group; // М[номер института]О-[номер группы][Б,М,А]-[год поступления]
+
+    private string? _practiceCourse;
+
+    public Student(
+        string name,
+        string lastName,
+        string middleName,
+        string group,
+        string practiceCourse)
+    {
+        Name = name;
+        LastName = lastName;
+        MiddleName = middleName;
+        Group = group;
+        PracticeCourse = practiceCourse;
+    }
+
+    //конструктор
+    public string Name
+    {
+        get { return _name; }
+
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException(nameof(value));
+            else _name = value;
+        }
+    }
+
+
+    // свойства
+    public string LastName
+    {
+        get { return _lastName; }
+
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException(nameof(value));
+            else _lastName = value;
+        }
+    }
+
+    public string MiddleName
+    {
+        get { return _middleName; }
+
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException(nameof(value));
+            else _middleName = value;
+        }
+    }
+
+    public string Group
+    {
+        get { return _group; }
+
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException(nameof(value));
+            else _group = value;
+        }
+    }
+
+    //
+    public string Course
+    {
+        get
+        {
+            return _group.Substring(4, 1);
+        }
+    }
+
+    public string PracticeCourse
+    {
+        get { return _practiceCourse; }
+
+        set
+        {
+            if (value is null)
+                throw new ArgumentNullException(nameof(value));
+            else _practiceCourse = value;
+        }
+    }
+
+
+
+    //ПЕРЕОРПЕДЕЛЕНИЕ МЕТОДОВ
+
+    //переопределение то стринг
+    public override string ToString()
+    {
+        
+        return $"[{_name} {_lastName} {_middleName}  " +
+            $"Group: {_group}, " +
+            $"Practice course: {_practiceCourse}. ]";
+    }
+
+    //переопределение гет хэш код
+    public override int GetHashCode()
+    {
+        return (((_name.GetHashCode() * 11 + _lastName.GetHashCode() ) * 31
+            + _middleName.GetHashCode() ) * 7 + _group.GetHashCode() ) * 3
+            + _practiceCourse.GetHashCode();
+    }
+
+
+
+    //переопределение глубокого сравнения
+    public override bool Equals(object? obj)
+    {
+        if (obj == null) return false;
+
+        if (obj is Student std) return Equals(std);
+
+        return false;
+    }
+
+    //определение для глубокого сравнения
+    public bool Equals(
+        Student std)
+    {
+        if (std == null) return false;
+
+        return _name.Equals(std._name)
+            && _lastName.Equals(std._lastName)
+            && _middleName.Equals(std._middleName)
+            && _group.Equals(std._group)
+            && _practiceCourse.Equals(std._practiceCourse);
+
+    }
+
+
+}
---
+++ Task1/Task1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Practice.Labs
+{
+    internal class Task1
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var me1 = new Student("Yana", "Kasyanova", "Stanislavovna", "М8О-211Б-21", "");
+            }catch (ArgumentNullException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            var me = new Student("Yana", "Kasyanova", "Stanislavovna", "М8О-211Б-21", "C#");
+
+            Console.WriteLine(me.ToString());
+
+            Console.WriteLine(me.GetHashCode());
+
+            Console.WriteLine(me.Equals(me));
+
+            Console.WriteLine($"Course: {me.Course}");
+
+            Console.WriteLine(me.Group);
+        } 
+    }
+}
